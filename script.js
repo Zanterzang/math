db29a147be1cb8068c00c9509e2a5ca5065d452a
Zanterzang <150271524+Zanterzang@users.noreply.github.com>
2025-02-23 01:36:37 +0600

    Update script.js
    Fixed parentheses bugs

--- script.js
+++ script.js
@@ -70,11 +70,8 @@ right.addEventListener("click", function() {
 })
 let calcs = [];
 let colcs = "";
-console.log(numbox.innerHtml)
 for (var i in btns) {
-    console.log(i);
     if (!((i === "length")||(i === "namedItem")||(i === "item")||(i==16)||(i==17))) {
-        console.log(btns[i].textContent);
         btns[i].addEventListener("click", function() {
             if (numbox.textContent === "Syntax Error"||numbox.textContent === "Math Error"||numbox.textContent.replace("|", "") === "0"||numbox.textContent.replace("|", "") === "+") {
                 clear();
@@ -92,36 +89,40 @@ document.getElementById("ac").addEventListener("click", function() {
 btns[17].addEventListener("click", calculate);
 function calculate() {
     calcs = [["("]];
-    colcs = numbox.textContent.replace("|", "")
+    colcs = numbox.textContent.replace("|", "");
     for (var c in colcs) {
         if (c==0) {
-            if (1*colcs[c]==colcs[c]) {
+            if (Number(colcs[c])) {
                 calcs.push(colcs[c]);
             } else if (colcs[c]==="*"||colcs[c]==="/"||colcs[c]===")") {
                 numbox.textContent = "Syntax Error";
                 console.log("here");
                 return;
-            } else if (colcs[c]==="-"||colcs[c]===["("]) {
-                calcs.push(colcs[c])
+            } else if (colcs[c]==="-") {
+                calcs.push(colcs[c]);
+            } else if (colcs[c]==="(") {
+                calcs.push([colcs[c]]);
             }
         } else {
-            if (calcs[calcs.length-1] == 1*calcs[calcs.length-1]) {
-                if (1*colcs[c]==colcs[c]) {
+            if (Number(calcs[calcs.length-1])) {
+                if (Number(colcs[c])||Number(colcs[c])===0) {
                     calcs[calcs.length-1] = calcs[calcs.length-1] + colcs[c];
-                } else if (!(colcs[c]===["("])) {
+                } else if (!(colcs[c]==="("||colcs[c]===")")) {
                     calcs.push(colcs[c]);
                 } else {
-                    calcs.push("*")
-                    calcs.push(colcs[c])
+                    if (colcs[c]==="(") {
+                        calcs.push("*");
+                    }
+                    calcs.push([colcs[c]]);
                 }
-            } else if (!(calcs[calcs.length-1]===[")"])) {
-                if (1*colcs[c] == colcs[c]) {
+            } else if (calcs[calcs.length-1][0] !== ")") {
+                if (Number(colcs[c])) {
                     if (calcs[calcs.length-1]==="-" && (!(Number(calcs[calcs.length-2])))) {
                         calcs[calcs.length-1] = calcs[calcs.length-1] + colcs[c];
                     } else {
                         calcs.push(colcs[c]);
                     }
-                } else if (colcs[c]==="*"||colcs[c]==="/"||colcs[c]===[")"]) {
+                } else if (colcs[c]==="*"||colcs[c]==="/"||colcs[c]===")") {
                     numbox.textContent = "Syntax Error";
                     console.log("here");
                     return;
@@ -133,24 +134,28 @@ function calculate() {
                     } else {
                         calcs.push("-");
                     }
+                } else if (colcs[c]==="(") {
+                    calcs.push([colcs[c]]);
                 }
             } else {
-                if (1*colcs[c] == colcs[c]||colcs[c]===["("]) {
+                if (Number(colcs[c])||colcs[c]==="(") {
                     calcs.push("*")
                 }
-                calcs.push(colcs[c]);
+                if (colcs[c]==="(") {
+                    calcs.push([colcs[c]]);
+                } else {
+                    calcs.push(colcs[c]);
+                }
             }
         }
     }
     calcs.push([")"]);
     brackets = [];
-    console.log(calcs)
     for (var i in calcs) {
         if (Array.isArray(calcs[i])||Array.isArray(calcs[i])) {
             brackets.push([calcs[i][0], i]);
         }
     }
-    console.log(brackets)
     var order = 0;
     for (var b in brackets) {
         if (brackets[b][0]===")") {
@@ -162,7 +167,7 @@ function calculate() {
             order++;
             var o = 1;
             for (o=1; o<=b; o++) {
-                if (brackets[1*b-o][0]==="(" & brackets[1*b-o].length == 2) {
+                if (brackets[1*b-o][0]==="(" && calcs[brackets[1*b-o][1]].length == 1) {
                     calcs[brackets[b][1]].push(order);
                     calcs[brackets[1*b-o][1]].push(order);
                     break;
@@ -173,11 +178,9 @@ function calculate() {
                 console.log("here");
                 return;
             }
-            console.log(brackets);
         }
     }
     for (var i in calcs) {
-        console.log(calcs[i])
         if ((Array.isArray(calcs[i]))&&!(calcs[i].length===2)) {
             numbox.textContent = "Syntax Error";
             console.log("here");
@@ -190,11 +193,12 @@ function calculate() {
         coords = [];
         for (var f in calcs) {
             if (Array.isArray(calcs[f])&&calcs[f][1]==i) {
-                coords.push(f)
+                coords.push(Number(f));
+                console.log(calcs[f]);
+                console.log(i)
             }
         }
         curstr = calcs.slice(coords[0]+1, coords[1]);
-        console.log(curstr);
         if (curstr.indexOf("*")!= -1) {
             while (curstr.indexOf("*") != -1) {
                 if (!(curstr[curstr.indexOf("*")+1])||!(curstr[curstr.indexOf("*")-1])) {
@@ -203,11 +207,9 @@ function calculate() {
                     return;
                 }
                 curstr.splice(curstr.indexOf("*")-1, 3, `${curstr[curstr.indexOf("*")-1]*curstr[curstr.indexOf("*")+1]}`);
-                console.log(curstr);
             }
         }
         if (curstr.indexOf("/")!= -1) {
-            console.log(curstr[curstr.indexOf("/")+1])
             while (curstr.indexOf("/") != -1) {
                 if (!(curstr[curstr.indexOf("/")+1])||!(curstr[curstr.indexOf("/")-1])) {
                     numbox.textContent = "Syntax Error";
@@ -218,11 +220,9 @@ function calculate() {
                     return;
                 }
                 curstr.splice(curstr.indexOf("/")-1, 3, `${curstr[curstr.indexOf("/")-1]/curstr[curstr.indexOf("/")+1]}`);
-                console.log(curstr);
             }
         }
         if (curstr.indexOf("+")!= -1) {
-            console.log(curstr[curstr.indexOf("+")+1])
             while (curstr.indexOf("+") != -1) {
                 if (!(curstr[curstr.indexOf("+")+1])||!(curstr[curstr.indexOf("+")-1])) {
                     numbox.textContent = "Syntax Error";
@@ -231,11 +231,9 @@ function calculate() {
                     return;
                 }
                 curstr.splice(curstr.indexOf("+")-1, 3, `${1*curstr[curstr.indexOf("+")-1]+curstr[curstr.indexOf("+")+1]*1}`);
-                console.log(curstr);
             }
         }
         if (curstr.indexOf("-")!= -1) {
-            console.log(curstr[curstr.indexOf("-")+1])
             while (curstr.indexOf("-") != -1) {
                 if (!(curstr[curstr.indexOf("-")+1])||!(curstr[curstr.indexOf("-")-1])) {
                     numbox.textContent = "Syntax Error";
@@ -243,12 +241,9 @@ function calculate() {
                     return;
                 }
                 curstr.splice(curstr.indexOf("-")-1, 3, `${1*curstr[curstr.indexOf("-")-1]-curstr[curstr.indexOf("-")+1]*1}`);
-                console.log(curstr);
             }
         }
-        console.log(calcs, coords[0], coords[1], coords[1]-coords[0]);
         calcs.splice(coords[0], coords[1]-coords[0]+1, curstr[0]);
-        console.log(calcs);
     }
     numbox.textContent = calcs[0];
 }
